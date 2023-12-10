4194f896998bb65757b5bdc0adc113cfe7a6a907
Vedat Kara <vedatkara@mail.com>
2023-12-10 19:04:00 +0300

    start

---
+++ src/main/java/com/randeu/randeu/RandeuApplication.java
@@ -0,0 +1,13 @@
+package com.randeu.randeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandeuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/randeu/randeu/RandeuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randeu.randeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
