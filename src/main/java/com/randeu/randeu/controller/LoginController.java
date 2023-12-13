8a89a4653b19154dc76cfaa43bd17f824d014c94
Barış Suna <barsuna02@hotmail.com>
2023-12-13 20:08:28 +0300

    Email and password received for authentication

--- src/main/java/com/randeu/randeu/controller/LoginController.java
+++ src/main/java/com/randeu/randeu/controller/LoginController.java
@@ -1,8 +1,14 @@
 package com.randeu.randeu.controller;
+import com.randeu.randeu.model.Person;
 import com.randeu.randeu.service.LoginService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
@@ -14,4 +20,12 @@ public class LoginController {
     public String index() {
         return "index";
     }
+
+    @PostMapping("/signIn")
+    public String userSignIn(@ModelAttribute Person person){
+        System.out.println(person.getEmail()+" "+person.getPassword());
+        return "index";
+    }
+
+
 }
