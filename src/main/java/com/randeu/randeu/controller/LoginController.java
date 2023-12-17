9a82a1ea5c8647d17c68e08db7fe43c74b424e13
Vedat Kara <vedatkara@mail.com>
2023-12-17 23:33:41 +0300

    Dashboard page created(tbc). User's info retrieved successfully.

--- src/main/java/com/randeu/randeu/controller/LoginController.java
+++ src/main/java/com/randeu/randeu/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.randeu.randeu.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,11 +21,15 @@ public class LoginController {
         return "index";
     }
 
-    @PostMapping("/randeu")
-    public String userSignIn(@ModelAttribute Person person){
-        loginService.authenticate(person.getEmail(), person.getPassword());
-        return "randeu";
+    @RequestMapping("/randeu")
+    public String userSignIn(@ModelAttribute Person person, Model model){
+        Person user = loginService.authenticate(person.getEmail(), person.getPassword()).getBody();
 
+        assert user != null;
+        model.addAttribute("name", user.getName());
+        model.addAttribute("surname", user.getSurname());
+
+        return "randeu";
     }
 
 
